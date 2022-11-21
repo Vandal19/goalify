4df00040dd6c64b9967da0664a2ddaa8437d8f8b
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-11-21 15:29:49 -0800

    Set up Header and add Icon for register and login

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <div className="container">
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          </div>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
+        </div>
       </Router>
     </>
   );
