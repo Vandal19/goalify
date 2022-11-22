8e73801c828ce7b2eac88fb04e6f1d1a692ffef8
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-11-21 17:07:19 -0800

    Successfully register a user to the backend

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -17,7 +17,7 @@ export const register = createAsyncThunk(
   "auth/register",
   async (user, thunkAPI) => {
     try {
-      return await authService.register();
+      return await authService.register(user);
     } catch (error) {
       const message =
         (error.response &&
