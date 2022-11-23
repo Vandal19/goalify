20c0959b7fa55e9f40598f6e2b79c7b8bf45364f
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-11-22 23:32:20 -0800

    Create goalslice and goalservice to get all goals

--- src/features/goals/goalSlice.js
+++ src/features/goals/goalSlice.js
@@ -26,7 +26,22 @@ export const createGoal = createAsyncThunk('goals/create', async (goalData, thun
   }
 })
 
-console.log("create Token", createGoal)
+// Get goals
+export const getGoals = createAsyncThunk('goals/getAll', async(_, thunkAPI) => {
+  try {
+    const token = thunkAPI.getState().auth.user.token
+    return await goalService.getGoals(token)
+  } catch (error) {
+    const message =
+      (error.response &&
+        error.response.data &&
+        error.response.data.message) ||
+      error.message ||
+      error.toString();
+    return thunkAPI.rejectWithValue(message);
+  }
+})
+
 
 export const goalSlice = createSlice({
   name: "goal",
@@ -45,6 +60,19 @@ export const goalSlice = createSlice({
         state.goals.push(action.payload);
       })
       .addCase(createGoal.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(getGoals.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getGoals.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.goals = action.payload;
+      })
+      .addCase(getGoals.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
